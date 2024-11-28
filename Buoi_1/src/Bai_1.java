8b181621ac0583f7f1ed0a05117f90460f425644
AlexZedred <wtf0888506598@gmail.com>
2024-11-28 18:17:53 +0700

    Nap file

--- Buoi_1/src/Bai_1.java
+++ Buoi_1/src/Bai_1.java
@@ -1,10 +1,9 @@
-package buoi_1;
 import java.util.Scanner;
-public class bai_1 {
+public class Bai_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = sc.nextLine();
-        System.out.println("Hello, " + name );
+        System.out.println("Hello, " + name);
     }
 }
