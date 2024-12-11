f184a9c87169e49f5dc091a9841826f922486917
AlexZedred <wtf0888506598@gmail.com>
2024-12-11 21:01:39 +0700

    Nap file

---
+++ C0924L1-module2/src/ss7_abstract_interface/Circle.java
@@ -0,0 +1,34 @@
+package ss7_abstract_interface;
+
+public class Circle implements Resizeable {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void resize(double percent) {
+        radius += radius * percent / 100;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", area=" + getArea() +
+                '}';
+    }
+}
---
+++ C0924L1-module2/src/ss7_abstract_interface/Main.java
@@ -0,0 +1,8 @@
+package ss7_abstract_interface;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        System.out.println(circle.toString());
+    }
+}
